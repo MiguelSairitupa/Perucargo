67015aa31e57fa6bbfe6786b4ff09c7630b8021e
MiguelSairitupa <msairitupa.231ds29@istta.edu.pe>
2025-05-12 09:45:16 -0500

    Corregir definicion de rutas para ConductorController en web

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConductorController;
 
 Route::get('/', function () {
-    Route::resource('conductores', ConductorController::class);
+    return view('welcome');
 });
+
+// Rutas para gestionar conductores
+Route::resource('conductores', ConductorController::class);
